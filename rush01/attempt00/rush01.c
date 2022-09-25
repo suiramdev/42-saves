c8e91288e38c734c032b5aa93975bfbd5c115610
Marius Nouchet <mnouchet@e3r7p9.clusters.42paris.fr>
2022-09-25 23:25:05 +0200

    Saved rush01 attempts

--- rush01/attempt00/rush01.c
+++ rush01/attempt00/rush01.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 13:51:18 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/09/25 20:35:30 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/09/25 17:39:01 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 #include <stdio.h>
 #include "ft_maths.h"
 #include "ft_std.h"
-
-int	GRID_SIZE = 4;
 
 // Verify that the clues were placed right 
 int	verify_clues(int *clues, int grid_size)
@@ -40,20 +38,20 @@ int	verify_clues(int *clues, int grid_size)
 // TODO: Check that there is the needed amount of clues, not less, not much
 // i != ft_sqrt(grid_size, 2) - 1 
 // on line 72 before the verify_clues calls, with a || operator
-int	*get_clues(char *arg)
+int	*get_clues(char *arg, int grid_size)
 {
 	int	*clues;
 	int	i;
 	int	j;
 
-	clues = malloc(ft_sqrt(GRID_SIZE, 2) * sizeof(int));
+	clues = malloc(ft_sqrt(grid_size, 2) * sizeof(int));
 	i = 0;
 	j = 0;
 	while (arg[i])
 	{
 		if (i % 2 == 0)
 		{
-			if (arg[i] < '1' || arg[i] > '0' + GRID_SIZE)
+			if (arg[i] < '1' || arg[i] > '0' + grid_size)
 				return (0);
 			clues[j++] = arg[i] - '0';
 		}
@@ -61,7 +59,7 @@ int	*get_clues(char *arg)
 			return (0);
 		i++;
 	}
-	if (!verify_clues(clues, GRID_SIZE))
+	if (!verify_clues(clues, grid_size))
 		return (0);
 	return (clues);
 }
@@ -119,42 +117,70 @@ void	first_grid(int **grid, int *clues)
 	}
 }
 
+// Prefill the grid with obvious values
+void	prefill_grid(int **grid, int *clues, int grid_size)
+{
+	int	row;
+	int	col;
+
+	row = 0;
+	col = 0;
+	while (row < grid_size)
+	{
+		while (col < grid_size)
+		{
+			if (row == 0 && clues[col] == 1)
+				grid[row][col] = 4;
+			else if (row == grid_size - 1 && clues[grid_size + col] == 1)
+				grid[row][col] = 4;
+			else if (col == 0 && clues[2 * grid_size + col] == 1)
+				grid[row][col] = 4;
+			else if (col == grid_size - 1 && clues[3 * grid_size + col] == 1)
+				grid[row][col] = 4;
+			col++;
+		}
+		row++;
+		col = 0;
+	}
+}
+
 // Create the game's grid_size
 // It also fills it with the already known numbers
-int	**init_grid(int *clues)
+int	**init_grid(int grid_size, int *clues)
 {
 	int	**grid;
 	int	row;
 	int	col;
 
-	grid = malloc(GRID_SIZE * sizeof(int *));
+	grid = malloc(grid_size * sizeof(int *));
 	row = 0;
-	while (row < GRID_SIZE)
+	col = 0;
+	while (row < grid_size)
 	{
-		col = 0;
-		grid[row] = malloc(GRID_SIZE * sizeof(int));
-		while (col < GRID_SIZE)
+		grid[row] = malloc(grid_size * sizeof(int));
+		while (col < grid_size)
 		{
 			grid[row][col] = 0;
 			col++;
 		}
 		row++;
+		col = 0;
 	}
 	first_grid(grid, clues);
 	//prefill_grid(grid, clues, grid_size);
 	return (grid);
 }
 
-int	is_number_available(int nb, int **grid, int pos)
+int	is_number_available(int nb, int **grid, int grid_size, int pos)
 {
 	int	row;
 	int	col;
 	int	i;
 
-	row = pos / GRID_SIZE;
-	col = pos % GRID_SIZE;
+	row = pos / grid_size;
+	col = pos % grid_size;
 	i = 0;
-	while (i < GRID_SIZE)
+	while (i < grid_size)
 	{
 		if (grid[row][i] == nb || grid[i][col] == nb)
 			return (0);
@@ -163,82 +189,77 @@ int	is_number_available(int nb, int **grid, int pos)
 	return (1);
 }
 
-int	is_row_filled(int **grid, int pos)
-{
-	int	row;
-	int	i;
-
-	row = pos / GRID_SIZE;
-	i = 0;
-	while (i < GRID_SIZE)
-	{
-		if (!grid[row][i])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	is_col_filled(int **grid, int pos)
-{
-	int	col;
-	int	i;
-
-	col = pos % GRID_SIZE;
-	i = 0;
-	while (i < GRID_SIZE)
-	{
-		if (!grid[i][col])
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	can_place(int nb, int **grid, int pos, int *clues)
-{
-	return (is_number_available(nb, grid, pos));
-}
-
-int	solve_grid(int **grid, int pos, int *clues)
+// Weird way to check visibility of skycrapers
+int	validate_row(int **grid, int *clues, int grid_size, int pos)
 {
 	int	row;
 	int	col;
-	int	nb;
+	int	i;
+	int	visible[4];
 
-	if (pos > ft_sqrt(GRID_SIZE, 2))
-		return (1);
-	row = pos / GRID_SIZE;
-	col = pos % GRID_SIZE;
-	nb = 1;
-	while (nb <= GRID_SIZE)
+	row = pos / grid_size;
+	col = pos % grid_size;
+	i = 0;
+	while (i < 4)
+		visible[i++] = 0;
+	i = 0;
+	while (i < grid_size)
 	{
-		if (can_place(nb, grid, pos, clues)) {
-			grid[row][pos] = nb;
+		visible[0] += grid[i][col] > grid[0][col];
+		visible[1] += grid[i][col] > grid[grid_size - 1][col];
+		visible[2] += grid[row][i] > grid[row][0];
+		visible[3] += grid[row][i] > grid[row][grid_size - 1];
+		i++;
+	}
+	return (visible[0] == clues[col]
+		&& visible[1] == clues[grid_size + col]
+		&& visible[2] == clues[2 * grid_size + row]
+		&& visible[3] == clues[3 * grid_size + row]);
+}
 
-			if (solve_grid(grid, pos + 1, clues))
-				return (1);
+// Fill the grid full of numbers.
+// TODO. Backtracking in it, when the rows aren't valid
+// (see validate_row)
+int	complete_grid(int **grid, int *clues, int grid_size, int pos)
+{
+	int	row;
+	int	col;
+	int	i;
+
+	if (pos >= ft_sqrt(grid_size, 2))
+		return (0);
+	row = pos / grid_size;
+	col = pos % grid_size;
+	if (grid[row][col] > 0)
+		return (complete_grid(grid, clues, grid_size, pos + 1));
+	i = 1;
+	while (i <= grid_size)
+	{
+		if (is_number_available(i, grid, grid_size, pos))
+		{
+			grid[row][col] = i;
+			//printf("%d\n", validate_row(grid, clues, grid_size, pos));
+			complete_grid(grid, clues, grid_size, pos + 1);
 		}
-		grid[row][col] = 0;
-		nb++;
+		i++;
 	}
 	return (0);
 }
 
 // Display the grid
-void	display_grid(int **grid)
+void	display_grid(int **grid, int grid_size)
 {
 	int	row;
 	int	col;
 
 	row = 0;
 	col = 0;
-	while (row < GRID_SIZE)
+	while (row < grid_size)
 	{
-		while (col < GRID_SIZE)
+		while (col < grid_size)
 		{
 			ft_putchar('0' + grid[row][col]);
-			if (col < GRID_SIZE - 1)
+			if (col < grid_size - 1)
 				ft_putchar(' ');
 			col++;
 		}
@@ -250,23 +271,21 @@ void	display_grid(int **grid)
 
 int	main(int argc, char **argv)
 {
+	int	grid_size;
 	int	**grid;
 	int	*clues;
 
-	if (argc < 2)
-	{
-		write(1, "Error", 5);
-		return (0);
-	}
-	clues = get_clues(argv[1]);
+	(void)argc;
+	grid_size = 4;
+	clues = get_clues(argv[1], grid_size);
 	if (!clues)
 	{
 		write(1, "Error", 5);
 		return (0);
 	}
-	grid = init_grid(clues);
-	solve_grid(grid, clues, 0);
-	display_grid(grid);
+	grid = init_grid(grid_size, clues);
+	complete_grid(grid, clues, grid_size, 0);
+	display_grid(grid, grid_size);
 	free(clues);
 	return (0);
 }
