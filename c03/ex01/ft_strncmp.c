222820f90d964db87d7a9135e80a095d462eb391
Marius Nouchet <mnouchet@e3r7p14.clusters.42paris.fr>
2022-09-29 12:26:34 +0200

    Pressed the panic button

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mnouchet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 11:57:23 by mnouchet          #+#    #+#             */
-/*   Updated: 2022/09/23 14:19:37 by mnouchet         ###   ########.fr       */
+/*   Updated: 2022/09/26 22:10:08 by mnouchet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (i < n && s1[i] == s2[i] && (s1[i] && s2[i]))
+	while (i < n - 1 && s1[i] == s2[i] && s1[i])
 		i++;
 	return (s1[i] - s2[i]);
 }
